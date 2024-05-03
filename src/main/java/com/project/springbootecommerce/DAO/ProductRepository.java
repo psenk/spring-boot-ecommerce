4a85a8847179632283ef11f99f105df0beb5633b
Tyler <psenk168@gmail.com>
2024-05-03 12:52:11 -0500

    lesson two

--- src/main/java/com/project/springbootecommerce/DAO/ProductRepository.java
+++ src/main/java/com/project/springbootecommerce/DAO/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.project.springbootecommerce.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.project.springbootecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
