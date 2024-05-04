d9fd4fe103ccf2cef5716fa1fb6335fc1689a9e4
Tyler <psenk168@gmail.com>
2024-05-03 20:55:41 -0500

    lesson four

--- src/main/java/com/project/springbootecommerce/DAO/ProductRepository.java
+++ src/main/java/com/project/springbootecommerce/DAO/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.project.springbootecommerce.DAO;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import com.project.springbootecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
 }
